be9da917fe83ac9c129b5eefbd98b308ef8f0d49
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-09-24 10:22:28 -0400

    Completed Question 1 which compares two employees' salaries.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -3,5 +3,15 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
 
+        double bobSalary = 19250;
+        double garySalary = 24552;
+
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        if (bobSalary == highestSalary) {
+            System.out.println("Bob's salary is higher");
+        } else {
+            System.out.println("Gary's salary is higher");
+        }
     }
 }
